6c8ce7928a23444d0bbf5fc88558441d2a89aecb
FlokiTV <dawysonw@gmail.com>
2025-02-21 18:05:55 -0300

    feat: add crud endpoints

--- src/api/clients/clients.module.ts
+++ src/api/clients/clients.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
+import { DrizzleModule } from 'src/drizzle/drizzle.module';
 
 @Module({
+  imports: [DrizzleModule],
   controllers: [ClientsController],
   providers: [ClientsService],
 })
--- src/api/clients/clients.service.ts
+++ src/api/clients/clients.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
+import { DRIZZLE } from 'src/drizzle/drizzle.module';
+import { DrizzleDB } from 'src/drizzle/types/drizzle';
+import { clients } from 'src/drizzle/schema/schema';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class ClientsService {
-  create(createClientDto: CreateClientDto) {
-    return 'This action adds a new client';
+  constructor(@Inject(DRIZZLE) private db: DrizzleDB) {}
+
+  async create(createClientDto: CreateClientDto) {
+    const data = await this.db
+      .insert(clients)
+      .values(createClientDto)
+      .returning();
+    return data[0];
   }
 
-  findAll() {
-    return `This action returns all clients`;
+  async findAll() {
+    const data = await this.db.select().from(clients);
+    return data;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} client`;
+  async findOne(id: number) {
+    const data = await this.db.select().from(clients).where(eq(clients.id, id));
+    if (data[0]) {
+      return data[0];
+    } else {
+      throw new ForbiddenException('Client not found');
+    }
   }
 
-  update(id: number, updateClientDto: UpdateClientDto) {
-    return `This action updates a #${id} client`;
+  async update(id: number, updateClientDto: UpdateClientDto) {
+    const data = await this.db
+      .update(clients)
+      .set(updateClientDto)
+      .where(eq(clients.id, id))
+      .returning();
+    if (data[0]) {
+      return data[0];
+    } else {
+      throw new ForbiddenException('Client not found');
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} client`;
+  async remove(id: number) {
+    const data = await this.db.delete(clients).where(eq(clients.id, id));
+    if (data.changes > 0) {
+      return { success: true };
+    } else {
+      return { success: false };
+    }
   }
 }
