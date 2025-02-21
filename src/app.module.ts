a3902ce3d883ab68739f814eb39de60a97383a1b
FlokiTV <dawysonw@gmail.com>
2025-02-21 18:10:36 -0300

    fix: remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './api/users/users.module';
 import { ClientsModule } from './api/clients/clients.module';
-import { AuthModule } from './api/auth/auth.module';
 import { DrizzleModule } from './drizzle/drizzle.module';
 
 @Module({
-  imports: [DrizzleModule, UsersModule, ClientsModule, AuthModule],
+  imports: [DrizzleModule, ClientsModule],
   controllers: [],
   providers: [],
 })
