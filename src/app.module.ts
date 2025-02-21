d639f1d4326b9b0b39f52297798273e7dcea3b43
FlokiTV <dawysonw@gmail.com>
2025-02-21 15:56:15 -0300

    feat: new auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './api/users/users.module';
 import { ClientsModule } from './api/clients/clients.module';
+import { AuthModule } from './api/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ClientsModule],
+  imports: [UsersModule, ClientsModule, AuthModule],
   controllers: [],
   providers: [],
 })
